daf10f2527ea672217830ef3e8dfe41a345f98ce
SamiAchnine <satejeda99@gmail.com>
2026-02-13 12:18:42 -0500

    added script in separate file

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Create an array */
+/* Create function that concatenates array into 
+a sentence (adding spaces between words), and 
+outputs the result to '<p id="output"><p>' innerHTML */
+/* Add an event listener to generateBtn.
+When clicked, the generateBtn should execute 
+the generateSentence function, displaying the sentence. */
